0db9120acf4503bfdf6c8fefba5ad867da7a652d
Imperi13 <imperi.night@gmail.com>
2022-08-12 21:02:08 +0900

    add warn

--- error.c
+++ error.c
@@ -46,3 +46,38 @@ void error_at(char *loc, char *fmt, ...) {
 void not_implemented(const char *msg) { error("not implemented: %s", msg); }
 
 void not_implemented_at(char *loc) { error_at(loc, "not implemented"); }
+
+void warn(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+}
+
+void warn_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  char *line = loc;
+  while (user_input < line && line[-1] != '\n')
+    line--;
+  char *end = loc;
+  while (*end != '\n')
+    end++;
+
+  int line_num = 1;
+  for (char *p = user_input; p < line; p++)
+    if (*p == '\n')
+      line_num++;
+
+  int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+  fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+  int pos = loc - line + indent;
+  fprintf(stderr, "%*s", pos, "");
+
+  char msg[0x100];
+  snprintf(msg, 0xff, fmt, ap);
+
+  fprintf(stderr, "^ %s\n", msg);
+}
