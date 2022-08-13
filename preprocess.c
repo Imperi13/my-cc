d3c9cbe3b3ba66d7b8fe8b83b73f0f1f8bb53350
Imperi13 <imperi.night@gmail.com>
2022-08-13 16:12:21 +0900

    impl ifdef ifndef

--- preprocess.c
+++ preprocess.c
@@ -6,6 +6,9 @@
 #include "preprocess.h"
 #include "tokenize.h"
 
+static void process_text_line(Token **post, Token **pre, Token *tok);
+static void consume_text_line(Token **pre, Token *tok);
+
 Define *define_list;
 
 Define *find_define(char *def_name, int def_len) {
@@ -15,18 +18,78 @@ Define *find_define(char *def_name, int def_len) {
   return NULL;
 }
 
-bool is_if_group(Token *tok) {
-  char *name = tok->next->str;
-  int len = tok->next->len;
-  return strncmp(name, "ifdef", len) == 0;
+bool cmp_ident(Token *tok, const char *name) {
+  return strncmp(tok->str, name, tok->len) == 0;
 }
 
-void process_if_group(Token **post, Token **pre, Token *tok) {}
+bool is_if_group(Token *tok) {
+  return cmp_ident(tok->next, "ifdef") || cmp_ident(tok->next, "ifndef");
+}
+
+void process_if_group(Token **post, Token **pre, Token *tok) {
+  consume(&tok, tok, "#");
+  if (cmp_ident(tok, "ifdef")) {
+    expect_kind(&tok, tok, TK_IDENT);
+
+    Token *cond_tok = consume_kind(&tok, tok, TK_IDENT);
+    bool cond = (find_define(cond_tok->str, cond_tok->len) != NULL);
+    expect_kind(&tok, tok, TK_NEWLINE);
+
+    Token *dummy = NULL;
+    while (!equal(tok, "#")) {
+      if (cond)
+        process_text_line(post, &tok, tok);
+      else
+        consume_text_line(&tok, tok);
+    }
+
+    expect(&tok, tok, "#");
+    if (!cmp_ident(tok, "endif"))
+      not_implemented_at("only ifdef~endif");
+    consume_kind(&tok, tok, TK_IDENT);
+    expect_kind(&tok, tok, TK_NEWLINE);
+
+    *pre = tok;
+  } else if (cmp_ident(tok, "ifndef")) {
+    expect_kind(&tok, tok, TK_IDENT);
+
+    Token *cond_tok = consume_kind(&tok, tok, TK_IDENT);
+    bool cond = (find_define(cond_tok->str, cond_tok->len) == NULL);
+    expect_kind(&tok, tok, TK_NEWLINE);
+
+    Token *dummy;
+    while (!equal(tok, "#")) {
+      if (cond)
+        process_text_line(post, &tok, tok);
+      else
+        consume_text_line(&tok, tok);
+    }
+
+    expect(&tok, tok, "#");
+    if (!cmp_ident(tok, "endif"))
+      not_implemented_at("only ifndef~endif");
+    consume_kind(&tok, tok, TK_IDENT);
+    expect_kind(&tok, tok, TK_NEWLINE);
+
+    *pre = tok;
+  } else {
+    not_implemented_at(tok->str);
+  }
+}
 
 void process_text_line(Token **post, Token **pre, Token *tok) {
   while (tok->kind != TK_NEWLINE) {
     (*post)->next = tok;
     (*post) = (*post)->next;
+    tok = tok->next;
+  }
+
+  // skip TK_NEWLINE
+  *pre = tok->next;
+}
+
+void consume_text_line(Token **pre, Token *tok) {
+  while (tok->kind != TK_NEWLINE) {
     tok = tok->next;
   }
 
