15d4e09ad5700519d8e223665ddd9fb2c0961cfe
Imperi13 <imperi.night@gmail.com>
2022-08-14 13:20:11 +0900

    fix test

--- franken_test.sh
+++ franken_test.sh
@@ -19,7 +19,9 @@ rm *.o
 
 for filepath in ${compile}; do
   ./build/mycc1 ${filepath} > ./build/${filepath%.c}1.s;
+  if [ $? -ne 0 ]; then { exit 1; }; fi
   ./build/mycc2 ${filepath} > ./build/${filepath%.c}2.s;
+  if [ $? -ne 0 ]; then { exit 1; }; fi
   diff ./build/${filepath%.c}1.s ./build/${filepath%.c}2.s;
   if [ $? -ne 0 ]; then { echo "FAIL";exit 1; }; else echo "PASS"; fi
 done
