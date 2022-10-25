3f68af48b6f5486d3d40837fd4ccbf1325a4ad04
Imperi13 <imperi.night@gmail.com>
2022-10-25 17:01:13 +0900

    add testcase

--- external/test_cases.sh
+++ external/test_cases.sh
@@ -654,3 +654,4 @@ run_test double_3 'int main(){double a = 10; double b = --a; if(a < 10 && b < 10
 run_test double_3 'int main(){double a = 10; double b = a--; if(a < 10 && b > 9) return 174; else return 164;}' 174
 
 run_test floating_cast 'int main(){float a = 10;double b = 20; if(29<a+b && a+b < 31) return 174;else return 164;}' 174
+run_test floating_cast 'int main(){double a = 10;float b = a; if(19<a+b && a+b < 21) return 174;else return 164;}' 174
